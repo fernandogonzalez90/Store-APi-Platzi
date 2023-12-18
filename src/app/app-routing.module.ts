47d68485d89e8c5db1afc8313581f2ba354a3f93
fernandogonzalez90 <fer.ema.gonzalez@gmail.com>
2023-12-18 14:35:58 -0300

    se añade ruta nueva.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreComponent } from './components/store/store.component';
+import { ViewProductoComponent } from './components/view-producto/view-producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: StoreComponent
+  },
+  {
+    path: 'producto/:id', component: ViewProductoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
